3e263501ac3696fb6c7a7c8f3118392c44305c54
Jennufer7w7 <73638811+Jennifer7w7@users.noreply.github.com>
2021-08-01 02:49:59 -0500

    Capitulo #6

---
+++ commands/utilidad/google-images.js
@@ -0,0 +1,37 @@
+const { MessageEmbed } = require('discord.js')
+const Discord = require('discord.js')
+const lineReply = require('discord-reply')
+const img = require('images-scraper')
+const google = new img({
+    puppeteer: {
+        handless: true,
+    }
+})
+
+module.exports = {
+    name: "image",
+    aliases: [""],
+    description: "Envia Una Imagen De Google :D",
+    usage: "texto",
+    run: async(client, message, args) => {
+        const query = args.join(" ")
+        if(!query) return message.lineReply('Por Favor Pon Una Busqueda :D')
+
+        var random = Math.floor((Math.random() * 90) + 0);
+        console.log(random)
+        const resutls = await google.scrape(query, 100)
+        const hasil = resutls[random].url
+        message.lineReply(`Generando Imagen....`)
+        let embedpic = new Discord.MessageEmbed()
+        .setTitle(`Imagen Generada COn Exito OwO`)
+        .setImage(hasil)
+        .setColor('RANDOM')
+
+        message.channel.startTyping()
+
+        setTimeout(() => {
+            message.lineReply(embedpic)
+            message.channel.stopTyping()
+        }, 6000)
+    }
+} //Volvemos en cuanto se instale images-scraper xD
